461d815c90426955297596d9a621c94e8bfd60d0
Igor Tkachuk <I.Tka4yk@gmail.com>
2021-06-26 20:43:29 +0300

    feat: add rote for edit item and add error handler for all routes if need it

--- routes/notes.js
+++ routes/notes.js
@@ -6,7 +6,6 @@ var data = require("../data/data.json");
 /* GET notes listing. */
 router.get("/", function (req, res, next) {
   res.send(data);
-  console.log("route '/'");
 });
 
 router.get("/stats", function (req, res, next) {
@@ -18,16 +17,15 @@ router.get("/stats", function (req, res, next) {
       };
       acc.push(categoryInstance);
     }
-    
+
     if (note._archived) {
       categoryInstance.archived = (categoryInstance.archived ?? 0) + 1;
     } else {
       categoryInstance.active = (categoryInstance.active ?? 0) + 1;
     }
-    
+
     return acc;
   }, []);
-  console.log("Stats", stats);
   res.send(stats);
 });
 
@@ -35,12 +33,17 @@ router.get("/:id", function (req, res, next) {
   let { id } = req.params;
   id = +id;
   const idx = data.findIndex((el) => el._id === id);
-  res.send(data[idx]);
-  console.log("route '/:id'");
+
+  if (idx != -1) {
+    res.send(data[idx]);
+    return;
+  }
+
+  res.sendStatus(404);
 });
 
-router.post("/", function(req, res, next){
-  const newNote = {...req.body };
+router.post("/", function (req, res, next) {
+  const newNote = { ...req.body };
   const created = new Date().toLocaleDateString();
   const _archived = false;
   const _id = Date.now();
@@ -49,17 +52,42 @@ router.post("/", function(req, res, next){
     ...newNote,
     created,
     _archived,
-    _id
+    _id,
   });
   res.send(data);
+});
+
+router.patch("/:id", function (req, res, next) {
+  const updData = { ...req.body };
+
+  let { id } = req.params;
+  id = +id;
+  const idx = data.findIndex((el) => el._id === id);
+
+  if (idx != -1) {
+    data[idx] = {
+      ...data[idx],
+      ...updData,
+    };
+    res.send(data[idx]);
+    return;
+  }
+
+  res.sendStatus(404);
 });
 
 router.delete("/:id", function (req, res, next) {
   let { id } = req.params;
   id = +id;
   const idx = data.findIndex((el) => el._id === id);
-  data.splice(idx, 1);
-  res.send(data);
+
+  if (idx != -1) {
+    data.splice(idx, 1);
+    res.send(data);
+    return;
+  }
+
+  res.sendStatus(404);
 });
 
 module.exports = router;
